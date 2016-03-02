961e0ff573ddd1bff0581f36493984c268bf4feb
Will Barnes <wtb2@rice.edu>
2016-03-02 16:07:57 -0600

    initial commit of utils from HYDRAD

---
+++ source/file.h
@@ -0,0 +1,18 @@
+// ****
+// *
+// * A simple routine to accurately read floating-point values
+// * from datafiles
+// * 
+// * (c) Dr. Stephen J. Bradshaw
+// *
+// * Date last modified: 15/04/2005
+// *
+// ****
+
+
+#include <string>
+#include <stdlib.h>
+
+void ReadDouble( void *pInputFile, double *pDouble );
+
+bool string2bool(std::string);
